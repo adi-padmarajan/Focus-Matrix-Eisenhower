e0c4bd4f9813e7322fa890bbfd5c5b266251da01
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-19 10:25:19 -0800

    Rendered the input tasks on the page

--- To-Do-App.js
+++ To-Do-App.js
@@ -1,10 +1,23 @@
 const todoList = [];
 
+renderTodoList();
+
+function renderTodoList(){
+    let todoListHTML = '';
+    for(let i = 0; i < todoList.length; i++){
+        const toDo = todoList[i];
+        const html = `<p>${toDo}</p>`;
+        todoListHTML += html;
+    }
+    console.log(todoListHTML);
+    document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+}
+
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value;
-    
     todoList.push(name);
     console.log(todoList);
     inputElement.value = ''; //To Clear the input text box once the task has been added
+    renderTodoList();
 }
