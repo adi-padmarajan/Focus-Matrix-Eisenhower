a9d62e62c9f37a92de153e528b4bdbaa50844ddc
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-19 19:12:44 -0800

    Implemented Priority Selection Feature

--- To-Do-App.js
+++ To-Do-App.js
@@ -6,10 +6,11 @@ function renderTodoList(){
     let todoListHTML = '';
     for(let i = 0; i < todoList.length; i++){
         const todoObject = todoList[i];
-        const {name, dueDate} = todoObject;
+        const {name, dueDate, priority} = todoObject;
         const html = `
             <div>${name}</div> 
             <div>${dueDate}</div>
+            <div>${priority}</div>
             <button onclick = "
                 todoList.splice(${i}, 1);
                 renderTodoList();
@@ -23,9 +24,13 @@ function renderTodoList(){
 function addTodo(){
     const inputElement = document.querySelector('.js-name-input');
     const dateInputElement = document.querySelector('.js-due-date-input');
+    const priorityElement = document.querySelector('.js-prioritize-tag');
+
     const name = inputElement.value;
     const dueDate = dateInputElement.value;
-    todoList.push({name, dueDate});
+    const priority = priorityElement.value;
+
+    todoList.push({name, dueDate, priority});
     inputElement.value = ''; //To Clear the input text box once the task has been added
     renderTodoList();
 }
