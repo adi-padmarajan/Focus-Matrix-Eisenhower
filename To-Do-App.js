149227cd8cb58f88dc2d56e11677e77c2945b71c
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-20 13:08:24 -0800

    Fixed Alignment Issues

--- To-Do-App.js
+++ To-Do-App.js
@@ -4,24 +4,28 @@ renderTodoList();
 
 function renderTodoList(){
     let todoListHTML = '';
+    if (todoList.length === 0){
+        document.querySelector('.js-todo-list').innerHTML = '<p class = "text-slate-500 text-sm italic"> No Tasks Scheduled </p>';
+        return;
+    }
     for(let i = 0; i < todoList.length; i++){
         const todoObject = todoList[i];
         const {name, dueDate, priority} = todoObject;
         const html = `
-            <div class="text-slate-700">${name}</div> 
-            <div class="text-slate-500">${dueDate}</div>
-            <div class="text-slate-600">${priority}</div>
+            <div class="text-slate-700 text-center">${name}</div> 
+            <div class="text-slate-500 text-center">${dueDate}</div>
+            <div class="text-slate-600 text-center">${priority}</div>
             <button
                 onclick="
                     todoList.splice(${i}, 1);
                     renderTodoList();
                 "
-                class="delete-todo-button bg-rose-600 hover:bg-rose-700 text-white rounded px-3 py-2 text-xs font-medium"
+                class="w-full delete-todo-button bg-rose-600 hover:bg-rose-700 text-white rounded px-3 py-2 text-xs font-medium"
             >
                 Delete
             </button> 
             <button
-                class="add-to-matrix bg-amber-500 hover:bg-amber-600 text-white rounded px-3 py-2 text-xs font-medium"
+                class=" w-full add-to-matrix bg-amber-500 hover:bg-amber-600 text-white rounded px-3 py-2 text-xs font-medium"
             >
                 Add to Matrix
             </button>
