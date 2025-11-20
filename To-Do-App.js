3bb10c7a37c6ff05d2f9a49b040a1ee2babf04d8
Aditya Padmarajan <aditya.padmarajan@gmail.com>
2025-11-20 13:28:45 -0800

    added a function to check for an empty task list

--- To-Do-App.js
+++ To-Do-App.js
@@ -4,10 +4,7 @@ renderTodoList();
 
 function renderTodoList(){
     let todoListHTML = '';
-    if (todoList.length === 0){
-        document.querySelector('.js-todo-list').innerHTML = '<p class = "text-slate-500 text-sm italic"> No Tasks Scheduled </p>';
-        return;
-    }
+    checkEmptyTaskList();
     for(let i = 0; i < todoList.length; i++){
         const todoObject = todoList[i];
         const {name, dueDate, priority} = todoObject;
@@ -19,6 +16,7 @@ function renderTodoList(){
                 onclick="
                     todoList.splice(${i}, 1);
                     renderTodoList();
+                    checkEmptyTaskList();
                 "
                 class="w-full delete-todo-button bg-rose-600 hover:bg-rose-700 text-white rounded px-3 py-2 text-xs font-medium"
             >
@@ -47,4 +45,13 @@ function addTodo(){
     todoList.push({name, dueDate, priority});
     inputElement.value = ''; //To Clear the input text box once the task has been added
     renderTodoList();
+}
+
+function checkEmptyTaskList(){
+    if(todoList.length === 0){
+        document.querySelector('.js-default-text').innerHTML = '<p class = "text-slate-500 text-sm italic text-center"> No Tasks Scheduled </p>';
+    }
+    else{
+        document.querySelector('.js-default-text').innerHTML = '';
+    }
 }
